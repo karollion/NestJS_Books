22eb7309b92ebab3d10010c6854a70e0995c7ac6
Karol Bernatowicz <karol.bernatowicz@gmail.com>
2023-12-19 08:36:13 +0100

    Add post and update to authors

--- src/authors/authors.controller.ts
+++ src/authors/authors.controller.ts
@@ -3,13 +3,15 @@ import {
   Get,
   Param,
   // Delete,
-  // Post,
-  // Put,
-  // Body,
+  Post,
+  Put,
+  Body,
   NotFoundException,
 } from '@nestjs/common';
 import { AuthorsService } from './authors.service';
 import { ParseUUIDPipe } from '@nestjs/common';
+import { CreateAuthorDTO } from './dtos/create-author.dto';
+import { UpdateAuthorDTO } from './dtos/update-author.dto';
 
 @Controller('authors')
 export class AuthorsController {
@@ -22,8 +24,25 @@ export class AuthorsController {
 
   @Get('/:id')
   async getById(@Param('id', new ParseUUIDPipe()) id: string) {
-    const prod = await this.authorsService.getById(id);
-    if (!prod) throw new NotFoundException('Author not found');
-    return prod;
+    const auth = await this.authorsService.getById(id);
+    if (!auth) throw new NotFoundException('Author not found');
+    return auth;
+  }
+
+  @Post('/')
+  create(@Body() authorData: CreateAuthorDTO) {
+    return this.authorsService.create(authorData);
+  }
+
+  @Put('/:id')
+  async update(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Body() authorData: UpdateAuthorDTO,
+  ) {
+    if (!(await this.authorsService.getById(id)))
+      throw new NotFoundException('Author not found');
+
+    await this.authorsService.updateById(id, authorData);
+    return { success: true };
   }
 }
--- src/authors/authors.service.ts
+++ src/authors/authors.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Author } from '@prisma/client';
 import { PrismaService } from 'src/shared/services/prisma.service';
+import { ConflictException } from '@nestjs/common';
 
 @Injectable()
 export class AuthorsService {
@@ -14,5 +15,34 @@ export class AuthorsService {
     return this.prismaService.author.findUnique({
       where: { id },
     });
+  }
+
+  // Omit tworzy nowy interfejs na podstawie Author ale bez "id" !!!
+  public async create(authorData: Omit<Author, 'id'>): Promise<Author> {
+    try {
+      return await this.prismaService.author.create({
+        data: authorData,
+      });
+    } catch (error) {
+      if (error.code === 'P2002')
+        throw new ConflictException('Name is already taken');
+      throw error;
+    }
+  }
+
+  public async updateById(
+    id: Author['id'],
+    authorData: Omit<Author, 'id'>,
+  ): Promise<Author> {
+    try {
+      return this.prismaService.author.update({
+        where: { id },
+        data: authorData,
+      });
+    } catch (error) {
+      if (error.code === 'P2002')
+        throw new ConflictException('Name is already taken');
+      throw error;
+    }
   }
 }
