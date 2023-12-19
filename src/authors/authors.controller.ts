b9b37789a8472002bbfdaa7a19977df4cd5f4ad9
Karol Bernatowicz <karol.bernatowicz@gmail.com>
2023-12-19 07:45:35 +0100

    Add getAll and getById to authors

--- src/authors/authors.controller.ts
+++ src/authors/authors.controller.ts
@@ -1,7 +1,29 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  // Delete,
+  // Post,
+  // Put,
+  // Body,
+  NotFoundException,
+} from '@nestjs/common';
 import { AuthorsService } from './authors.service';
+import { ParseUUIDPipe } from '@nestjs/common';
 
 @Controller('authors')
 export class AuthorsController {
   constructor(private authorsService: AuthorsService) {}
+
+  @Get('/')
+  getAll(): any {
+    return this.authorsService.getAll();
+  }
+
+  @Get('/:id')
+  async getById(@Param('id', new ParseUUIDPipe()) id: string) {
+    const prod = await this.authorsService.getById(id);
+    if (!prod) throw new NotFoundException('Author not found');
+    return prod;
+  }
 }
